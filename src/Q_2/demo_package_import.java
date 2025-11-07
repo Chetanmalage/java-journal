71876c3e379a115f5fec467f5495dbb3bc267f98
chetanmalage <chetanmalage234@gmail.com>
2025-11-07 22:30:00 +0530

    added new package

---
+++ src/Q_2/demo_package_import.java
@@ -0,0 +1,16 @@
+package Q_2;
+
+import Q_1.Animal;
+
+public class demo_package_import implements Animal {
+
+	public void sound() {
+		// TODO Auto-generated method stub
+		
+	}	public void sleep() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/Q_2/package-info.java
@@ -0,0 +1,1 @@
+package Q_2;
